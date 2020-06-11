c4e5a7566ef736c15548bad28eb96bfc7a433463
Julieta Parra <julieta.victoria.vargas@gmail.com>
2020-06-11 09:03:18 -0500

    Modulo prenomina

--- modulos/rh/prenominas/index.php
+++ modulos/rh/prenominas/index.php
@@ -1,32 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>  
+        <link href="../prenominas/styles/navbar.css" rel="stylesheet" type="text/css">
+        <script src="../prenominas/styles/navbar2.js"></script>
+        <script src="../prenominas/styles/jquery.js"></script>  
     </head>
 
     <body>
-    <div class="container-fluid">
+    <div>
     <!-- Second navbar for categories -->
-    <nav class="navbar navbar-default">
-      <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Sistema RH</a>
-        </div>
-    
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
+    <nav>
+      <div>
+        <div>
+          <ul id="button">
             <li><a href="../gridprenominas/index.php";>Prenómina</a></li>
-            <li><a href="#">Sobrerecibo</a></li>
+            <li><a href="../sobrerecibo/index.php">Sobrerecibo</a></li>
             <li><a href="#">Comparador CONTPAQi® vs Sistema RH</a></li>
             <li><a href="#">Reporteador</a></li>
             <li><a href="#">Histórico</a></li>
