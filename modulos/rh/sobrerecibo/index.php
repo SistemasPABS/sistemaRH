467bf570a6281c4853dea4dd0ba4ea13be33fc37
Julieta Parra <julieta.victoria.vargas@gmail.com>
2020-08-02 17:24:52 -0500

    Bug fixed el boton - en el nombre de la persona no hacia nada, ya funciona

--- modulos/rh/sobrerecibo/index.php
+++ modulos/rh/sobrerecibo/index.php
@@ -150,6 +150,14 @@ if($row3= pg_fetch_array($result3)){
                 $(this).parents("tr").remove();
 		        $(".add-new").removeAttr("disabled");
             });
+            $(document).ready(function () {
+                $(".tbtn").click(function () {
+                    $(this).parents(".custom-table").find(".toggler1").removeClass("toggler1");
+                    $(this).parents("tbody").find(".toggler").addClass("toggler1");
+                    $(this).parents(".custom-table").find(".fa-minus-circle").removeClass("fa-minus-circle");
+                    $(this).parents("tbody").find(".fa-plus-circle").addClass("fa-minus-circle");
+                });
+            });
     </script>
 
     
