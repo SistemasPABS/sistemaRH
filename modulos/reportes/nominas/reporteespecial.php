0cc1e64116b76591ca8286cba643c5bf044ad94b
Julieta Parra <julieta.victoria.vargas@gmail.com>
2020-08-05 09:50:48 -0500

    Cambios en el config

--- modulos/reportes/nominas/reporteespecial.php
+++ modulos/reportes/nominas/reporteespecial.php
@@ -1,5 +1,4 @@
 <?php
-require __DIR__ . "/vendor/autoload.php";
 include_once ('../../../config/cookie.php');
 include_once ('../../../config/conectasql.php');
 $us_id=$_SESSION['us_id'];
@@ -8,15 +7,4 @@ $con->abre_conexion("0");
 $conexion=$con->conexion;
 $idnom=base64_decode($_GET['idnom']);//idnom
 
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
-
-$spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', 'Hello World !');
-
-$writer = new Xlsx($spreadsheet);
-$writer->save('hello world.xlsx');
-
-exit;
 ?>
