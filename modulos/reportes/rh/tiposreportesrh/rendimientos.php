07be710bc187c845ee836c6629ab0c287405ba7d
admin <admin@localhost.localdomain>
2020-08-10 11:17:07 -0500

    reportegaby

--- modulos/reportes/rh/tiposreportesrh/rendimientos.php
+++ modulos/reportes/rh/tiposreportesrh/rendimientos.php
@@ -9,15 +9,28 @@ ini_set('display_startup_errors', TRUE);
 date_default_timezone_set('America/Mexico_City');
 if (PHP_SAPI == 'cli')
 die('This example should only be run from a Web Browser');
-$idnom=base64_decode($_GET['idnom']);
+$fechainicio=base64_decode($_GET['fechainicio']);
+$fechafin=base64_decode($_GET['fechafin']);
+
 include ('../../../../config/conectasql.php');
 $exporta = new conectasql();
 $exporta->abre_conexion("0");
-$sqlxls="SELECT DISTINCT nombrecompleto FROM vw_general_personas_por_nomina_scpd WHERE nom_id = $idnom";
+$sqlxls="SELECT * from personas where persona_registro_dia between '$fechainicio' and '$fechafin'";
  /** Include PHPExcel */
 require_once ('../../../../librerias/phpexcel/Classes/PHPExcel.php');
+/*$objPHPExcel = new PHPExcel_Worksheet_Drawing();
+                    $objPHPExcel->setName('imgNotice');
+                    $objPHPExcel->setDescription('Noticia');
+                    $img = '../../../../images/logo.png'; // Provide path to your logo file
+                    $objPHPExcel->setPath($img);
+                    $objPHPExcel->setOffsetX(28);    // setOffsetX works properly
+                    $objPHPExcel->setOffsetY(300);  //setOffsetY has no effect
+                    $objPHPExcel->setCoordinates('A1');
+                    $objPHPExcel->setHeight(150); // logo height
+                    $objPHPExcel->setWorksheet($objPHPExcel->setActiveSheetIndex($pos+1)); */
 // Create new PHPExcel object*/
 $objPHPExcel = new PHPExcel();
+
 // Set document properties*/
     $objPHPExcel->getProperties()
             ->setCreator("Jaime Nieto")
@@ -28,10 +41,13 @@ $objPHPExcel = new PHPExcel();
             ->setKeywords("office 2007 openxml php")
             ->setCategory("Test result file");
             // Add some data*/
-    
+            $objPHPExcel->setActiveSheetIndex(0)
+            ->setCellValue('D1','Fecha de Registro')
+            ->setCellValue('E1','Hora de Registro');
+
 //se ejecuta la consulta
     $resultxls=pg_query($exporta->conexion,$sqlxls);
-    $a=8;
+    $a=2;
 
     if($rowxls=pg_fetch_array($resultxls)){
         $objPHPExcel->setActiveSheetIndex(0);
@@ -39,45 +55,18 @@ $objPHPExcel = new PHPExcel();
         do{
            
             $objPHPExcel->setActiveSheetIndex(0)
-                    ->setCellValue('D'.$a, $rowxls['nombrecompleto'])
-                    ->setCellValue('E'.$a, $rowxls['sal_monto_con']);
+                    ->setCellValue('D'.$a, $rowxls['persona_registro_dia'])
+                    ->setCellValue('E'.$a, $rowxls['persona_registro_hora']);
                     $a++;
         }while ($rowxls=pg_fetch_array($resultxls));
         
+    }else {
+        $objPHPExcel->setActiveSheetIndex(0)
+                    ->setCellValue('D'.$a, 'No existieron registros en ese rango de fechas');
     }
-
-    //Obteniendo la columna con el ultimo registro
-    $highestColumm = $objPHPExcel->setActiveSheetIndex(0)->getHighestColumn();
-    $objPHPExcel->setActiveSheetIndex(0)
-    ->setCellValue('M7',$highestColumm);
-
-    $fila = 1;
-    $columnamaxima = $highestColumm;
-
-    $objPHPExcel->setActiveSheetIndex(0);
-    for($column = $columnamaxima; $column != 'IW'; $column++){ 
-    $objPHPExcel->setActiveSheetIndex(0) ->setCellValue('O1',$column);}
-
-
-    //Iniciando bucle para llenar desde la columna "G" y consulta para llenar las comisiones
-
-    $querycomisiones = "SELECT DISTINCT co_nombre from vw_comnom where nom_id = $idnom";
-    $resultcomisiones = pg_query($exporta->conexion,$querycomisiones);
-    if($mostrarcomisiones = pg_fetch_array($resultcomisiones)){
-        do{
-            for($x='F'; $x != 'IW'; $x++) { 
-                $objPHPExcel->setActiveSheetIndex(0) 
-                ->setCellValue($x . '7', $mostrarcomisiones); 
-            }
-        }while($mostrarcomisiones = pg_fetch_array($resultcomisiones));
-    }
-    
-    
-
-    
    
     // Rename worksheet*/
-    $objPHPExcel->getActiveSheet()->setTitle('Detallado de nomina');
+    $objPHPExcel->getActiveSheet()->setTitle('Rendimientos de Personas');
 
 
     // Set active sheet index to the first sheet, so Excel opens this as the first sheet*/
@@ -86,7 +75,7 @@ $objPHPExcel = new PHPExcel();
 
     // Redirect output to a client’s web browser (Excel5)*/
     header('Content-Type: application/vnd.ms-excel');
-    header('Content-Disposition: attachment;filename="Detallado_de_nomina.xls"');
+    header('Content-Disposition: attachment;filename="Rendimientos_de_registros_de_personas.xls"');
     header('Cache-Control: max-age=0');
     // If you're serving to IE 9, then the following may be needed*/
     header('Cache-Control: max-age=1');
